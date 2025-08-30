b2b226ab155b9216e7c0672f1bc21ba0c2d68bad
abhisheksamriya <abhisheksamriya2004@gmail.com>
2025-08-31 01:55:29 +0530

    seo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.neptura.online/",
+  siteUrl: "https://www.neptura.online",
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  generateIndexSitemap: true,
 };
